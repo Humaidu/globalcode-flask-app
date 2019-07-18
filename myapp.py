a2101397ea60eef0c26a31deb6cc5d6f95385a54
Humaidu <humaiduali@gmail.com>
2019-07-18 14:51:48 +0100

    Running my app on a server called Heroku

--- myapp.py
+++ myapp.py
@@ -6,10 +6,6 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/notiform')
-def notiform():
-    return render_template('notiform.html')
-
  
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
